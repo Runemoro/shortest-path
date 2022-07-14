05d77051365f521d0e3210ca25a1bc2ddc8ef5a6
cyberlarino <larino@protonmail.com>
2022-07-14 20:02:55 +0200

    Refactor OrdinalDirection enum somewhat

--- src/main/java/shortestpath/pathfinder/OrdinalDirection.java
+++ src/main/java/shortestpath/pathfinder/OrdinalDirection.java
@@ -3,35 +3,46 @@ package shortestpath.pathfinder;
 import java.awt.Point;
 
 public enum OrdinalDirection {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
-    NORTH_WEST,
-    NORTH_EAST,
-    SOUTH_EAST,
-    SOUTH_WEST;
-
-    public Point toPoint() {
-        switch (this) {
-            case NORTH:
-                return new Point(0, 1);
-            case EAST:
-                return new Point(1, 0);
-            case SOUTH:
-                return new Point(0, -1);
-            case WEST:
-                return new Point(-1, 0);
-            case NORTH_WEST:
-                return new Point(-1, 1);
-            case NORTH_EAST:
-                return new Point(1, 1);
-            case SOUTH_EAST:
-                return new Point(1, -1);
-            case SOUTH_WEST:
-                return new Point(-1, -1);
-            default:
-                return new Point(0, 0);
+    NORTH {
+        public Point toPoint() {
+            return new Point(0, 1);
         }
-    }
+    },
+    EAST {
+        public Point toPoint() {
+            return new Point(1, 0);
+        }
+    },
+    SOUTH {
+        public Point toPoint() {
+            return new Point(0, -1);
+        }
+    },
+    WEST {
+        public Point toPoint() {
+            return new Point(-1, 0);
+        }
+    },
+    NORTH_WEST {
+        public Point toPoint() {
+            return new Point(-1, 1);
+        }
+    },
+    NORTH_EAST {
+        public Point toPoint() {
+            return new Point(1, 1);
+        }
+    },
+    SOUTH_EAST {
+        public Point toPoint() {
+            return new Point(1, -1);
+        }
+    },
+    SOUTH_WEST {
+        public Point toPoint() {
+            return new Point(-1, -1);
+        }
+    };
+
+    public abstract Point toPoint();
 }
