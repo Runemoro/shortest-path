Runemoro/shortest-path

commit b57d79bfc2ded948195b68d645bd26024fe5d817

World map tile counter hover tooltip
Added a tooltip on the world map that displays the path tile number from start to finish for the hovered tile

src/main/java/shortestpath/PathMapOverlay.java
@@ -85,7 +85,6 @@ public class PathMapOverlay extends Overlay {
         }
 
         if (plugin.getPathfinder() != null) {
-            graphics.setColor(plugin.getPathfinder().isDone() ? config.colourPath() : config.colourPathCalculating());
             List<WorldPoint> path = plugin.getPathfinder().getPath();
             for (int i = 0; i < path.size(); i++) {
                 WorldPoint point = path.get(i);
@@ -118,6 +117,14 @@ public class PathMapOverlay extends Overlay {
         final int height = end.getY() - y;
         x -= width / 2;
         y -= height / 2;
+
+        Point cursorPos = client.getMouseCanvasPosition();
+        Color tileColour = plugin.getPathfinder().isDone() ? config.colourPath() : config.colourPathCalculating();
+        if (cursorPos.getX() >= x && cursorPos.getX() <= (end.getX() - width / 2) &&
+            cursorPos.getY() >= y && cursorPos.getY() <= (end.getY() - width / 2)) {
+            tileColour = tileColour.darker();
+        }
+        graphics.setColor(tileColour);
 
         if (point.distanceTo(offset) > 1) {
             graphics.setStroke(new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0));

src/main/java/shortestpath/PathMapTooltipOverlay.java
@@ -1,0 +1,121 @@
+package shortestpath;
+
+import com.google.inject.Inject;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.Arrays;
+import java.util.List;
+import net.runelite.api.Client;
+import net.runelite.api.Point;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.widgets.WidgetID;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.ui.FontManager;
+import net.runelite.client.ui.JagexColors;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.OverlayPriority;
+
+public class PathMapTooltipOverlay extends Overlay {
+    private static final int TOOLTIP_OFFSET_HEIGHT = 25;
+    private static final int TOOLTIP_OFFSET_WIDTH = 15;
+    private static final int TOOLTIP_PADDING_HEIGHT = 1;
+    private static final int TOOLTIP_PADDING_WIDTH = 2;
+    private static final int TOOLTIP_TEXT_OFFSET_HEIGHT = -2;
+
+    private final Client client;
+    private final ShortestPathPlugin plugin;
+    private final ShortestPathConfig config;
+
+    @Inject
+    private PathMapTooltipOverlay(Client client, ShortestPathPlugin plugin, ShortestPathConfig config) {
+        this.client = client;
+        this.plugin = plugin;
+        this.config = config;
+        setPosition(OverlayPosition.DYNAMIC);
+        setPriority(OverlayPriority.LOW);
+        setLayer(OverlayLayer.MANUAL);
+        drawAfterInterface(WidgetID.WORLD_MAP_GROUP_ID);
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        if (!config.drawMap() || client.getWidget(WidgetInfo.WORLD_MAP_VIEW) == null) {
+            return null;
+        }
+
+        if (plugin.getPathfinder() != null) {
+            List<WorldPoint> path = plugin.getPathfinder().getPath();
+            Point cursorPos = client.getMouseCanvasPosition();
+            for (int i = 0; i < path.size(); i++) {
+                if (drawTooltip(graphics, cursorPos, path.get(i), i + 1)) {
+                    return null;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private boolean drawTooltip(Graphics2D graphics, Point cursorPos, WorldPoint point, int n) {
+        Point start = plugin.mapWorldPointToGraphicsPoint(point);
+        Point end = plugin.mapWorldPointToGraphicsPoint(point.dx(1).dy(-1));
+
+        if (start == null || end == null) {
+            return false;
+        }
+
+        int width = end.getX() - start.getX();
+
+        if (cursorPos.getX() < (start.getX() - width / 2) || cursorPos.getX() > (end.getX() - width / 2) ||
+            cursorPos.getY() < (start.getY() - width / 2) || cursorPos.getY() > (end.getY() - width / 2)) {
+            return false;
+        }
+
+        List<String> rows = Arrays.asList("Shortest path:", "Step " + n + " of " + plugin.getPathfinder().getPath().size());
+
+        graphics.setFont(FontManager.getRunescapeFont());
+        FontMetrics fm = graphics.getFontMetrics();
+        int tooltipHeight = fm.getHeight();
+        int tooltipWidth = rows.stream().map(fm::stringWidth).max(Integer::compareTo).get();
+
+        int clippedHeight = tooltipHeight + TOOLTIP_PADDING_HEIGHT * 2;
+        int clippedWidth = tooltipWidth + TOOLTIP_PADDING_WIDTH * 2;
+
+        Rectangle worldMapBounds = client.getWidget(WidgetInfo.WORLD_MAP_VIEW).getBounds();
+        int worldMapRightBoundary = worldMapBounds.width + worldMapBounds.x;
+        int worldMapBottomBoundary = worldMapBounds.height + worldMapBounds.y;
+
+        int drawPointX = start.getX() + TOOLTIP_OFFSET_WIDTH;
+        int drawPointY = start.getY();
+        if (drawPointX + clippedWidth > worldMapRightBoundary) {
+            drawPointX = worldMapRightBoundary - clippedWidth;
+        }
+        if (drawPointY + clippedHeight > worldMapBottomBoundary) {
+            drawPointY = start.getY() - TOOLTIP_OFFSET_HEIGHT * 2 - tooltipHeight;
+        }
+        drawPointY += TOOLTIP_OFFSET_HEIGHT;
+
+        Rectangle tooltipRect = new Rectangle(
+            drawPointX - TOOLTIP_PADDING_WIDTH,
+            drawPointY - TOOLTIP_PADDING_HEIGHT,
+            tooltipWidth + TOOLTIP_PADDING_WIDTH * 2,
+            tooltipHeight * rows.size() + TOOLTIP_PADDING_HEIGHT * 2);
+
+        graphics.setColor(JagexColors.TOOLTIP_BACKGROUND);
+        graphics.fillRect(tooltipRect.x, tooltipRect.y, tooltipRect.width, tooltipRect.height);
+
+        graphics.setColor(JagexColors.TOOLTIP_BORDER);
+        graphics.drawRect(tooltipRect.x, tooltipRect.y, tooltipRect.width, tooltipRect.height);
+
+        graphics.setColor(JagexColors.TOOLTIP_TEXT);
+        for (int i = 0; i < rows.size(); i++) {
+            graphics.drawString(rows.get(i), drawPointX, drawPointY + TOOLTIP_TEXT_OFFSET_HEIGHT + (i + 1) * tooltipHeight);
+        }
+
+        return true;
+    }
+}

src/main/java/shortestpath/ShortestPathPlugin.java
@@ -91,6 +91,9 @@ public class ShortestPathPlugin extends Plugin {
     private PathMapOverlay pathMapOverlay;
 
     @Inject
+    private PathMapTooltipOverlay pathMapTooltipOverlay;
+
+    @Inject
     private SpriteManager spriteManager;
 
     @Inject
@@ -131,6 +134,7 @@ public class ShortestPathPlugin extends Plugin {
         overlayManager.add(pathOverlay);
         overlayManager.add(pathMinimapOverlay);
         overlayManager.add(pathMapOverlay);
+        overlayManager.add(pathMapTooltipOverlay);
     }
 
     @Override
@@ -138,6 +142,7 @@ public class ShortestPathPlugin extends Plugin {
         overlayManager.remove(pathOverlay);
         overlayManager.remove(pathMinimapOverlay);
         overlayManager.remove(pathMapOverlay);
+        overlayManager.remove(pathMapTooltipOverlay);
     }
 
     @Subscribe

commit 8a2f80834477bb3891c6289c67fb9a0808d36b00

Fix collision map drawing regression
A collision map drawing regression was introduced in b57d79bfc2ded948195b68d645bd26024fe5d817, fully preventing the collision map from being shown when no active paths were available. The hover colour also had to be reset/updated more frequently.

src/main/java/shortestpath/PathMapOverlay.java
@@ -59,7 +59,7 @@ public class PathMapOverlay extends Overlay {
             for (int x = extent.x; x < (extent.x + extent.width + 1); x++) {
                 for (int y = extent.y - extent.height; y < (extent.y + 1); y++) {
                     if (plugin.getMap().isBlocked(x, y, z)) {
-                        drawOnMap(graphics, new WorldPoint(x, y, z));
+                        drawOnMap(graphics, new WorldPoint(x, y, z), false);
                     }
                 }
             }
@@ -85,25 +85,27 @@ public class PathMapOverlay extends Overlay {
         }
 
         if (plugin.getPathfinder() != null) {
+            Color colour = plugin.getPathfinder().isDone() ? config.colourPath() : config.colourPathCalculating();
             List<WorldPoint> path = plugin.getPathfinder().getPath();
             for (int i = 0; i < path.size(); i++) {
+                graphics.setColor(colour);
                 WorldPoint point = path.get(i);
                 WorldPoint last = (i > 0) ? path.get(i - 1) : point;
-                drawOnMap(graphics, point);
                 if (point.distanceTo(last) > 1) {
-                    drawOnMap(graphics, last, point);
+                    drawOnMap(graphics, last, point, true);
                 }
+                drawOnMap(graphics, point, true);
             }
         }
 
         return null;
     }
 
-    private void drawOnMap(Graphics2D graphics, WorldPoint point) {
-        drawOnMap(graphics, point, point.dx(1).dy(-1));
+    private void drawOnMap(Graphics2D graphics, WorldPoint point, boolean checkHover) {
+        drawOnMap(graphics, point, point.dx(1).dy(-1), checkHover);
     }
 
-    private void drawOnMap(Graphics2D graphics, WorldPoint point, WorldPoint offset) {
+    private void drawOnMap(Graphics2D graphics, WorldPoint point, WorldPoint offset, boolean checkHover) {
         Point start = plugin.mapWorldPointToGraphicsPoint(point);
         Point end = plugin.mapWorldPointToGraphicsPoint(offset);
 
@@ -118,18 +120,16 @@ public class PathMapOverlay extends Overlay {
         x -= width / 2;
         y -= height / 2;
 
-        Point cursorPos = client.getMouseCanvasPosition();
-        Color tileColour = plugin.getPathfinder().isDone() ? config.colourPath() : config.colourPathCalculating();
-        if (cursorPos.getX() >= x && cursorPos.getX() <= (end.getX() - width / 2) &&
-            cursorPos.getY() >= y && cursorPos.getY() <= (end.getY() - width / 2)) {
-            tileColour = tileColour.darker();
-        }
-        graphics.setColor(tileColour);
-
         if (point.distanceTo(offset) > 1) {
             graphics.setStroke(new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0));
             graphics.drawLine(start.getX(), start.getY(), end.getX(), end.getY());
         } else {
+            Point cursorPos = client.getMouseCanvasPosition();
+            if (checkHover &&
+                cursorPos.getX() >= x && cursorPos.getX() <= (end.getX() - width / 2) &&
+                cursorPos.getY() >= y && cursorPos.getY() <= (end.getY() - width / 2)) {
+                graphics.setColor(graphics.getColor().darker());
+            }
             graphics.fillRect(x, y, width, height);
         }
     }